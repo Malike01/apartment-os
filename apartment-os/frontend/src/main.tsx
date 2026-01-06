a0ade40d4cb4966898da02becc6b226cd095f9f5
Malike Sen <malikesen61@gmail.com>
2026-01-06 19:08:05 +0300

    bugfix: fixed browser route and inline css

--- apartment-os/frontend/src/main.tsx
+++ apartment-os/frontend/src/main.tsx
@@ -5,35 +5,39 @@ import App from "./App.tsx";
 import { ConfigProvider } from "antd";
 import { COLORS, LAYOUT_CONFIG, FONT_SIZES } from "./constants";
 import "./utils/chartSetup";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: COLORS.PRIMARY,
-          colorSuccess: COLORS.SUCCESS,
-          colorWarning: COLORS.WARNING,
-          colorError: COLORS.ERROR,
-          colorInfo: COLORS.INFO,
-          borderRadius: LAYOUT_CONFIG.BORDER_RADIUS,
-          fontFamily:
-            "'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif",
-          fontSize: parseInt(FONT_SIZES.SM),
-        },
-        components: {
-          Layout: {
-            colorBgBody: COLORS.BACKGROUND.BODY,
-            colorBgHeader: COLORS.BACKGROUND.CONTAINER,
+    <BrowserRouter>
+
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: COLORS.PRIMARY,
+            colorSuccess: COLORS.SUCCESS,
+            colorWarning: COLORS.WARNING,
+            colorError: COLORS.ERROR,
+            colorInfo: COLORS.INFO,
+            borderRadius: LAYOUT_CONFIG.BORDER_RADIUS,
+            fontFamily:
+              "'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif",
+            fontSize: parseInt(FONT_SIZES.SM),
           },
-          Card: {
-            colorBgContainer: COLORS.BACKGROUND.CONTAINER,
-            borderRadiusLG: LAYOUT_CONFIG.BORDER_RADIUS,
+          components: {
+            Layout: {
+              colorBgBody: COLORS.BACKGROUND.BODY,
+              colorBgHeader: COLORS.BACKGROUND.CONTAINER,
+            },
+            Card: {
+              colorBgContainer: COLORS.BACKGROUND.CONTAINER,
+              borderRadiusLG: LAYOUT_CONFIG.BORDER_RADIUS,
+            },
           },
-        },
-      }}
-    >
-      <App />
-    </ConfigProvider>
+        }}
+      >
+        <App />
+      </ConfigProvider>
+    </BrowserRouter>
   </StrictMode>
 );
